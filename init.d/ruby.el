2d8970644348c3cb28124659e160464fa5c84f31
Arne Brasseur <arne.brasseur@gmail.com>
2012-12-21 12:05:04 +0100

    Ruby : load rspec,rvm and rcodetools

--- init.d/ruby.el
+++ init.d/ruby.el
@@ -17,9 +17,40 @@
 (setq ruby-indent-level 2)
 (setq ruby-deep-indent-paren nil)       ; inside parameter lists, just indent two spaces rather than up to paren
 
-(require 'rspec-mode)
-(require 'rvm)
-(setq rspec-use-rake-flag nil)
-(setq rspec-spec-command "rspec")
-(custom-set-variables '(rspec-key-command-prefix (kbd "s-s")))
-(define-key rspec-mode-keymap (kbd "S-s") 'rspec-verify-single)
+(defun plexus-load-rspec ()
+  (progn
+    (require 'rspec-mode)
+    (setq rspec-use-rake-flag nil)
+    (setq rspec-spec-command "rspec")
+    (custom-set-variables '(rspec-key-command-prefix (kbd "s-s")))
+    (define-key rspec-mode-keymap (kbd "S-s") 'rspec-verify-single)))
+
+(defun plexus-set-rct-elisp-path ()
+  (setq load-path 
+        (append 
+         (list (concat user-emacs-directory "elisp/rcodetools"))
+         load-path)))
+
+(defun plexus-set-rct-bin-path ()
+  (setenv "PATH"
+          (concat user-emacs-directory "elisp/rcodetools/bin" ":" (concat (getenv "PATH")))))
+
+(defun plexus-set-rct-load-path ()
+  (setenv 
+   "RUBYLIB"
+   (concat user-emacs-directory "elisp/rcodetools/lib" ":" (concat (getenv "RUBYLIB")))))
+
+(defun plexus-activate-rcodetools ()
+  (progn
+    (plexus-set-rct-elisp-path)
+    (plexus-set-rct-bin-path)
+    (plexus-set-rct-load-path)
+    (require 'rcodetools)
+    (define-key ruby-mode-map (kbd "C-c C-c") 'xmp)))
+
+(eval-after-load 'ruby-mode
+  '(progn 
+     (require 'rvm)
+     (rvm-use-default)
+     (plexus-activate-rcodetools)
+     (plexus-load-rspec)))
