612bfc75ba70d6e6ffe2d4e036c79714984ebf53
Arne Brasseur <arne@arnebrasseur.net>
2013-03-05 22:00:55 +0100

    Use ido when calling M-x

---
+++ init.d/ido-M-x.el
@@ -0,0 +1,11 @@
+;; use "interactively do things" for M-x
+
+(global-set-key
+ "\M-x"
+ (lambda ()
+   (interactive)
+   (call-interactively
+    (intern
+     (ido-completing-read
+      "M-x "
+      (all-completions "" obarray 'commandp))))))
