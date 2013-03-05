7b6f054bad07dbaf45c92dfa5f01b396933730eb
Arne Brasseur <arne@arnebrasseur.net>
2013-03-05 22:00:17 +0100

    Look and feel : back to default theme, disable menu and scroll bar

--- init.d/look-and-feel.el
+++ init.d/look-and-feel.el
@@ -1,8 +1,11 @@
 ;; Startup and UI
 
 (setq inhibit-splash-screen t)                  ; no splash screen
-;(menu-bar-mode -1)                             ; no menu bar
+
+(menu-bar-mode -1)                              ; no menu bar
 (tool-bar-mode -1)                              ; no tool bar
+(scroll-bar-mode -1)                            ; no scroll bar
+
 (setq initial-scratch-message nil)              ; empty *scratch* buffer
 
 ;; Mode line
@@ -22,14 +25,14 @@
        (concat user-emacs-directory "elisp/color-theme-6.6.0")
        load-path))
 
-(require 'color-theme)
-(color-theme-initialize)
+;(require 'color-theme)
+;(color-theme-initialize)
 
 ; Loading both gives a light theme with a nice red mode line
 ;(color-theme-arjen)
 ;(color-theme-scintilla)
 
-(color-theme-charcoal-black)   ;dark
+;(color-theme-charcoal-black)   ;dark
 ;(color-theme-feng-shui)        ;light
 ;(color-theme-sitaramv-nt)      ;light
 ;(color-theme-arjen)
