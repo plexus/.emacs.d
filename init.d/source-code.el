c1a2b9f706cb07762ac7d62d6cd647e618bafcdc
Arne Brasseur <arne@arnebrasseur.net>
2013-03-15 10:17:21 +0100

    Enable column-number-mode in source files, consider (e)lisp files source files.

--- init.d/source-code.el
+++ init.d/source-code.el
@@ -1,17 +1,12 @@
 (add-hook 'ruby-mode-hook (lambda () (source-mode-init)))
 (add-hook 'sh-mode-hook (lambda () (source-mode-init)))
+(add-hook 'emacs-lisp-mode-hook (lambda () (source-mode-init)))
+(add-hook 'lisp-mode-hook (lambda () (source-mode-init)))
 
 (defun source-mode-init ()
   (progn
     (linum-mode 1)
-    (local-set-key (kbd "s-c") 'comment-region)
-    (local-set-key (kbd "s-u") 'uncomment-region)
-    (local-set-key (kbd "s-i") 'indent-region)
-    (local-set-key (kbd "M-s-e") 'align-to-equals)
-    (local-set-key (kbd "M-s-r") 'align-to-rocket)
-    (local-set-key (kbd "M-s-c") 'align-to-comma)
-    (local-set-key (kbd "M-s-s") 'align-to-semicolon)
-))
+    (column-number-mode)
 
     (local-set-key (kbd "H-c") 'comment-region)
     (local-set-key (kbd "H-u") 'uncomment-region)
