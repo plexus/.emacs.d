15cd6cd61aedd34f11a017c4761125004a37c2ab
Arne Brasseur <arne@arnebrasseur.net>
2013-07-26 22:04:38 +0200

    haskell-mode init

---
+++ init.d/haskell.el
@@ -0,0 +1,4 @@
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
