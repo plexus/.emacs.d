fe9371d3777f461daf1de0f8af28369115d933b0
Arne Brasseur <arne@arnebrasseur.net>
2020-12-23 12:24:47 +0100

    WIP

---
+++ init.el
@@ -0,0 +1,19 @@
+(defvar bootstrap-version)
+(setq straight-use-package-by-default t)
+
+(let ((install-url "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el")
+      (bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer (url-retrieve-synchronously install-url 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(use-package corgi
+  :straight (corgi
+             :type git
+             :branch "main"
+             :repo "/home/arne/github/lambdaisland/corgi"))
